e5f6a959376371e64cc67bdf6dcf28b180d3d3af
Konstantin <elagink@mail.ru>
2023-04-25 14:04:37 +0500

    Initial commit

---
+++ app/src/main/java/com/example/javalearningexampledatatransferbetweenactivity/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.javalearningexampledatatransferbetweenactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nameIn; //Поле имя
+    private EditText addressIn; //Поле адреса
+    private EditText emailIn; //Поле электронного адреса
+    private Button button; //Поле кнопки
+
+    private String name;
+    private String address;
+    private String email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Привязка к полям
+        nameIn = findViewById(R.id.nameIn);
+        addressIn = findViewById(R.id.addressIn);
+        emailIn = findViewById(R.id.emailIn);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(listener); //Обработка нажатия кнопки
+
+    }
+
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //Переключение на другой экран
+
+            //Считывание данных с экрана
+            name = nameIn.getText().toString();
+            address = addressIn.getText().toString();
+            email = emailIn.getText().toString();
+
+            //Создание намерения переключения на другой экран
+            Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+
+            //Запаковка данных в контейнер намерения
+            intent.putExtra("name", name);
+            intent.putExtra("address", address);
+            intent.putExtra("email", email);
+
+            //Старт переключения активности
+            startActivity(intent);
+
+        }
+    };
+
+}
---
+++ app/src/main/java/com/example/javalearningexampledatatransferbetweenactivity/SecondActivity.java
@@ -0,0 +1,56 @@
+package com.example.javalearningexampledatatransferbetweenactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    private TextView dataMainActivity; //Поле вывода информации из главного экрана
+    private Button button; //Поле кнопки
+
+    private String name; //поле хранения имени
+    private String address; //поле хранения адреса
+    private String email; //поле хранения почты
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        //Привязка к полям
+        dataMainActivity = findViewById(R.id.dataMainActivity);
+        button = findViewById(R.id.button);
+
+        //Считывание данных из главной активности
+        Bundle bundleIntent = getIntent().getExtras();
+
+        //Инициализация данных
+        name = bundleIntent.get("name").toString(); //Считывание и нинициализация имени
+        address = bundleIntent.get("address").toString(); //Считывание и нинициализация адреса
+        email = bundleIntent.get("email").toString(); //Считывание и нинициализация почты
+
+        //Вывод на экран данных из главной активности
+        dataMainActivity.setText("Имя: " + name + "\n"
+                                    + "Адрес: " + address + "\n"
+                                    + "Электронная почта: " + email + "\n");
+
+        button.setOnClickListener(listener); //Обработка нажатия кнопки
+    }
+
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //Возврат обратно на главную активность
+
+            //Создание намерения переключения на другой экран
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent); //Старт переключения активности
+
+        }
+    };
+
+}
